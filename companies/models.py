0edf7af3f1ff95d9f618ac6a515601df33a4d250
pedrolibas <pedrogv03@hotmail.com>
2023-01-28 02:53:13 -0300

    feat: registration of companies created

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    store_owner = models.CharField(max_length=255)
+    store_name = models.CharField(max_length=255)
--- file/serializers.py
+++ file/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import File
 from utils.transform import convert_file
 import os
+from companies.serializers import CompanySerializer
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,7 +12,12 @@ class FileSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         file = File.objects.create(**validated_data)
         converted_file = convert_file()
-        print(converted_file)
+        
+        for information in converted_file:
+            company = dict(store_owner = information.pop("store_owner"), store_name = information.pop("store_name"))
+            serializer = CompanySerializer(data=company)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
         if os.path.isfile('data_file/CNAB.txt'):
             os.remove('data_file/CNAB.txt')
