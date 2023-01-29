2d20794ead7ce1ca1db79a4c89dcbdff5f0712cc
pedrolibas <pedrogv03@hotmail.com>
2023-01-29 00:56:33 -0300

    feat: url correction and company listing creation

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+class CompanyView(generics.ListAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
--- file/urls.py
+++ file/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FileView
 
 urlpatterns = [
-    path("teste/", FileView.as_view())
+    path("file/", FileView.as_view())
 ]
